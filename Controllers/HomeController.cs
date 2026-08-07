84df7a7f8449c095adf7ab86e433bcee3249df3e
l4zon-keys <zamaliyema13@gmail.com>
2026-08-07 22:46:47 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using LoginFormASPCore6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LoginFormASPCore6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MyDbContext context;
+
+        public HomeController(MyDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                return RedirectToAction("Dashboard");
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(User u)
+        {
+            var myUser = context.Users.Where(x => x.Email == u.Email && x.Password == u.Password).FirstOrDefault();
+            if (myUser != null)
+            {
+                HttpContext.Session.SetString("UserSession", myUser.EmpName);
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.Message = "Bad Credential Email or Password.";
+            }
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            List<SelectListItem> Gender = new()
+            {
+                new SelectListItem {Value="Male",Text="Male"},
+                new SelectListItem {Value="Female",Text="Female"}
+            };
+            ViewBag.Gender = Gender;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Signup(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                await context.Users.AddAsync(u);
+                await context.SaveChangesAsync();
+                TempData["Success"] = "Registered Successfully";
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                ViewData["MySession"] = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                HttpContext.Session.Remove("UserSession");
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+This repository includes Login and SignUp with session using Asp .Net Core 6 MVC and Entity Framework Core.
+-
+- I have created Razor pages Login & Signup Dashboard in ASP.NET CORE 6 MVC.
+- SQL SERVER Database i have used.
+- For Database Operation & Connectivity i have used Entity Framework Core.
+- For Lgin & Logout i have used Session.
+
+
+<h3>Watch the video : </h3>
+
+https://github.com/pawankumar-vishwakarma/LoginAndSignUpPage/assets/58881689/6b69c55f-8426-433a-a15e-117a1529894e
+
+
+<h3>Login</h3>
+  <div align="center"> <img src="https://raw.githubusercontent.com/pawankumar-vishwakarma/LoginAndSignUpPage/master/wwwroot/images/Login.png"> </div>
+  
+<h3>Signup</h3>
+  <div align="center"> <img src="https://raw.githubusercontent.com/pawankumar-vishwakarma/LoginAndSignUpPage/master/wwwroot/images/Signup.png"> </div>
+
+<h3>Signup with Data</h3>
+  <div align="center"> <img src="https://raw.githubusercontent.com/pawankumar-vishwakarma/LoginAndSignUpPage/master/wwwroot/images/SignupData.png"> </div>
+
+<h3>Signup Successfully</h3>
+  <div align="center"> <img src="https://raw.githubusercontent.com/pawankumar-vishwakarma/LoginAndSignUpPage/master/wwwroot/images/SignupSuccess.png"> </div>
+
+<h3>Database</h3>
+  <div align="center"> <img src="https://raw.githubusercontent.com/pawankumar-vishwakarma/LoginAndSignUpPage/master/wwwroot/images/Database.png"> </div>
+
+<h3>Login with Credentials</h3>
+  <div align="center"> <img src="https://raw.githubusercontent.com/pawankumar-vishwakarma/LoginAndSignUpPage/master/wwwroot/images/LoginData.png"> </div>
+
+<h3>Dashboard (Login Successfully)</h3>
+  <div align="center"> <img src="https://raw.githubusercontent.com/pawankumar-vishwakarma/LoginAndSignUpPage/master/wwwroot/images/Dashboard.png"> </div>
